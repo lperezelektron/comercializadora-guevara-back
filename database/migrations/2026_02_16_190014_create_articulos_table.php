91369104cfcb0439924f658cfdf7cb3bbc5cc057
Santos Vallecillo <94139701+SantosAEA@users.noreply.github.com>
2026-02-16 23:23:39 -0600

    Carga inicial

---
+++ database/migrations/2026_02_16_190014_create_articulos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('nombre_corto');
+            $table->string('unidad', 10);
+            $table->boolean('activo')->default(true);
+            $table->string('imagen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articulos');
+    }
+};
